7be40bd81294aa129315e96e798d8e6fce35cc53
Ryan Murray <rymurr@dremio.com>
2020-01-29 22:50:58 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Prometheus exporter for Dremio metrics
+[![Build Status](https://travis-ci.org/rymurr/dremio-prometheus-exporter.svg?branch=master)](https://travis-ci.org/dremio-hub/dremio-prometheus-exporter)
+
+This runs a [prometheus](https://https://prometheus.io/) endpoint which publishes all dremio metrics to Prometheus
+
+## Build
+
+1. change parent pom version in `pom.xml` to your dremio version (see https://github.com/dremio/dremio-oss to find the correct version) 
+1. `mvn clean install` 
+1. move `dremio-telemetry-cloudwatch-{version}-shaded.jar` to the `jars` directory in your dremio installation
+1. restart dremio
+
+## configuration
+
+The file `dremio-telemetry.yaml` must exist on the classpath for your dremio installation and must contain the following
+
+```yaml
+metrics:
+  - name: cloudwatch_reporter
+    comment: >
+      Publish metrics on cloudwatch
+    reporter:
+    type: cloudwatch
+    port: 12543
+```
+
+An example has been provided in the root of this repo
---
+++ src/main/java/com/dremio/metrics/cloudwatch/CloudwatchConfigurator.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright (C) 2017-2019 Dremio Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.dremio.metrics.cloudwatch;
+
+import java.io.IOException;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+import com.codahale.metrics.MetricFilter;
+import com.codahale.metrics.MetricRegistry;
+import com.dremio.telemetry.api.config.ReporterConfigurator;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.google.common.base.Objects;
+
+import io.github.azagniotov.metrics.reporter.cloudwatch.CloudWatchReporter;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.cloudwatch.CloudWatchAsyncClient;
+import software.amazon.awssdk.services.cloudwatch.model.StandardUnit;
+
+/**
+ * Configurator for JMX
+ */
+@JsonTypeName("cloudwatch")
+public class CloudwatchConfigurator extends ReporterConfigurator {
+
+    private final Region region;
+    private final TimeUnit rateUnit;
+    private final TimeUnit durationUnit;
+    private long intervalMs;
+    private CloudWatchReporter cloudWatchReporter;
+
+    @JsonCreator
+    public CloudwatchConfigurator(@JsonProperty("region") String region,
+                                  @JsonProperty("rate") TimeUnit rateUnit,
+                                  @JsonProperty("duration") TimeUnit durationUnit,
+                                  @JsonProperty("intervalMs") long intervalMs) {
+        super();
+        this.region = Region.of(region.replace('-','_').toUpperCase());
+        this.rateUnit = Optional.ofNullable(rateUnit).orElse(TimeUnit.SECONDS);
+        this.durationUnit = Optional.ofNullable(durationUnit).orElse(TimeUnit.MILLISECONDS);
+        this.intervalMs = intervalMs;
+    }
+
+    @Override
+    public void configureAndStart(String name, MetricRegistry registry, MetricFilter filter) {
+        final CloudWatchAsyncClient amazonCloudWatchAsync =
+                CloudWatchAsyncClient
+                        .builder()
+                        .region(region)
+                        .build();
+
+        // todo percentiles and meter units
+        cloudWatchReporter =
+                CloudWatchReporter.forRegistry(registry, amazonCloudWatchAsync, CloudwatchConfigurator.class.getName())
+                        .convertRatesTo(rateUnit)
+                        .convertDurationsTo(durationUnit)
+                        .filter(filter)
+                        .withPercentiles(CloudWatchReporter.Percentile.P75, CloudWatchReporter.Percentile.P99)
+                        .withOneMinuteMeanRate()
+                        .withFiveMinuteMeanRate()
+                        .withFifteenMinuteMeanRate()
+                        .withMeanRate()
+                        .withArithmeticMean()
+                        .withStdDev()
+                        .withStatisticSet()
+                        .withZeroValuesSubmission()
+                        .withReportRawCountValue()
+                        .withHighResolution()
+                        .withMeterUnitSentToCW(StandardUnit.BYTES)
+                        .withJvmMetrics()
+                        .withGlobalDimensions("Region="+region.id(), "Instance=stage")
+                        .withDryRun()
+                        .build();
+
+        cloudWatchReporter.start(intervalMs, TimeUnit.MILLISECONDS);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        CloudwatchConfigurator that = (CloudwatchConfigurator) o;
+        return intervalMs == that.intervalMs &&
+                Objects.equal(region, that.region) &&
+                rateUnit == that.rateUnit &&
+                durationUnit == that.durationUnit &&
+                Objects.equal(cloudWatchReporter, that.cloudWatchReporter);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(region, rateUnit, durationUnit, intervalMs, cloudWatchReporter);
+    }
+
+    @Override
+    public void close() {
+        if (cloudWatchReporter != null) {
+            cloudWatchReporter.stop();
+        }
+    }
+}
